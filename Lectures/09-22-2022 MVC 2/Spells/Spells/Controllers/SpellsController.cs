4c51f0117b8a7e73908b26d6e1138996e0195b92
Steven Yackel <spazard1@gmail.com>
2022-09-22 15:10:15 -0500

    adds mvc 2 lecture

--- Lectures/09-22-2022 MVC 2/Spells/Spells/Controllers/SpellsController.cs
+++ Lectures/09-22-2022 MVC 2/Spells/Spells/Controllers/SpellsController.cs
@@ -1,14 +1,69 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
 using Spells.Models;
+using Spells.Services;
 using System.Diagnostics;
+using System.Net;
 
 namespace Spells.Controllers
 {
     public class SpellsController : Controller
     {
+
+        private static SpellsDatabase spells = new SpellsDatabase();
+
         public IActionResult Index()
         {
-            return View();
+            return View(spells);
+        }
+
+        public IActionResult ViewSpell(string id)
+        {
+            if (int.TryParse(id, out int result))
+            {
+                ViewData["id"] = result;
+                return View(spells.Get(result));
+            }
+
+            return RedirectToAction("index");
+        }
+
+        [HttpPost]
+        public IActionResult Add()
+        {
+            if (Request.HasFormContentType)
+            {
+                if (Request.Form.TryGetValue("spellName", out StringValues result))
+                {
+                    spells.Add(result.ToString());
+                    return RedirectToAction("index");
+                }
+            }
+
+            return StatusCode((int)HttpStatusCode.UnsupportedMediaType);
+        }
+
+        [HttpPost]
+        public IActionResult Delete()
+        {
+            if (!Request.HasFormContentType)
+            {
+                return RedirectToAction("index");
+            }
+
+            if (!Request.Form.TryGetValue("spellIndex", out StringValues result))
+            {
+                return RedirectToAction("index");
+            }
+
+            if (!int.TryParse(result, out int spellIndex))
+            {
+                return RedirectToAction("index");
+            }
+
+            spells.Delete(spellIndex);
+
+            return RedirectToAction("index");
         }
     }
 }
---
+++ Lectures/09-22-2022 MVC 2/Spells/Spells/Services/SpellsDatabase.cs
@@ -0,0 +1,33 @@
+﻿using Spells.Models;
+
+namespace Spells.Services
+{
+    public class SpellsDatabase
+    {
+        private List<SpellModel> spells = new() {
+            new SpellModel() { Spell = "Steven" },
+            new SpellModel() { Spell = "Amanda" },
+            new SpellModel() { Spell = "Benji" },
+        };
+
+        public SpellModel Get(int index)
+        {
+            return spells[index];
+        }
+
+        public int Count()
+        {
+            return spells.Count();
+        }
+
+        public void Add(string newSpell)
+        {
+            spells.Add(new SpellModel() { Spell = newSpell });
+        }
+
+        public void Delete(int id)
+        {
+            spells.RemoveAt(id);
+        }
+    }
+}
