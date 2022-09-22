dc491a858cd62ceb92dd0cf0765180ac938c6475
Steven Yackel <spazard1@gmail.com>
2022-09-22 13:58:04 -0500

    fix spells/home controller names

--- Lectures/09-22-2022 MVC 2/Spells/Spells/Controllers/SpellsController.cs
+++ Lectures/09-22-2022 MVC 2/Spells/Spells/Controllers/SpellsController.cs
@@ -4,29 +4,11 @@ using System.Diagnostics;
 
 namespace Spells.Controllers
 {
-    public class HomeController : Controller
+    public class SpellsController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
