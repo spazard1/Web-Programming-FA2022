bc85f534661ba642dd72c1e50d69e0a21b6d5438
Steven Yackel <spazard1@gmail.com>
2022-09-22 13:54:03 -0500

    adds base lecture for mvc 2

---
+++ Lectures/09-22-2022 MVC 2/Spells/Spells/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Spells.Models;
+using System.Diagnostics;
+
+namespace Spells.Controllers
+{
+    public class SpellsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
