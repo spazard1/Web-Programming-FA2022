f2648776a4f3e22a9cca11d56ac0e8c51894f297
Steven Yackel <styackel@microsoft.com>
2022-09-27 10:33:27 -0500

    base DI project updated

---
+++ Lectures/09-27-2022 Dependency Injection/Hobbits/Services/RequestIdGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hobbits.Services
+{
+    public class RequestIdGenerator
+    {
+        public string RequestId { get; }
+
+        public RequestIdGenerator()
+        {
+            this.RequestId = Guid.NewGuid().ToString();
+        }
+    }
+}
