36b3a5cf117115e3243badd2505ad78dd42b136a
Steven Yackel <styackel@microsoft.com>
2022-09-27 10:33:27 -0500

    base DI project updated

---
+++ Lectures/09-27-2022 Dependency Injection/Hobbits/Services/TimeOfDayProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hobbits.Services
+{
+    public class TimeOfDayProvider
+    {
+        public DateTime Current { get; } = DateTime.UtcNow;
+    }
+}
