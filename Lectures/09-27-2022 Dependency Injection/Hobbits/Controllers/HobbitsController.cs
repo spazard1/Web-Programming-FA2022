1496b89c17cb5b5bcfb5ee0d8940594d4f4a460c
Steven Yackel <styackel@microsoft.com>
2022-09-27 10:33:51 -0500

    fix controller route

--- Lectures/09-27-2022 Dependency Injection/Hobbits/Controllers/HobbitsController.cs
+++ Lectures/09-27-2022 Dependency Injection/Hobbits/Controllers/HobbitsController.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 namespace Hobbits.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class HobbitsController : Controller
     {
 
