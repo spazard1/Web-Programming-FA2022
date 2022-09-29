b3b761e2e209098e66617d036b5e9b9632fcc30a
Steven Yackel <spazard1@gmail.com>
2022-09-29 14:46:57 -0500

    adds filters lecture

---
+++ Lectures/09-29-2022 Filters/Hobbits/Filters/RequestIdFilter.cs
@@ -0,0 +1,25 @@
+﻿using Hobbits.Services;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Hobbits.Filters
+{
+    public class RequestIdFilter : IActionFilter
+    {
+        private readonly RequestIdGenerator requestIdGenerator;
+
+        public RequestIdFilter(RequestIdGenerator requestIdGenerator)
+        {
+            this.requestIdGenerator = requestIdGenerator;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            context.HttpContext.Response.Headers["request-id"] = requestIdGenerator.RequestId;
+        }
+    }
+}
---
+++ Lectures/09-29-2022 Filters/Hobbits/Filters/RequestLoggingFilter.cs
@@ -0,0 +1,25 @@
+﻿using Hobbits.Services;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Hobbits.Filters
+{
+    public class RequestLoggingFilter : IActionFilter
+    {
+        private readonly IHobbitLogger hobbitLogger;
+
+        public RequestLoggingFilter(IHobbitLogger hobbitLogger)
+        {
+            this.hobbitLogger = hobbitLogger;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            this.hobbitLogger.WriteLine("Ending an action");
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            this.hobbitLogger.WriteLine(context.HttpContext.Request.Method + "-" + context.HttpContext.Request.Path);
+        }
+    }
+}
