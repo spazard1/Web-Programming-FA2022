9cfae0859aa30679423c42843e3e85811610aecc
Steven Yackel <spazard1@gmail.com>
2022-10-04 14:49:44 -0500

    adds mario async/await lecture

---
+++ Lectures/10-04-2022 async await/Mario/Mario/Services/ExternalMarioService.cs
@@ -0,0 +1,20 @@
+﻿using Mario.Entities;
+using Newtonsoft.Json;
+using System.Runtime.CompilerServices;
+
+namespace Mario.Services
+{
+    public class ExternalMarioService
+    {
+
+        private readonly HttpClient httpClient = new();
+
+        public async Task<MarioEntity?> GetMarioLevelStatusAsync()
+        {
+            var response = await httpClient.GetAsync("https://webprogrammingmario.azurewebsites.net/api/mario/jump");
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<MarioEntity>(responseString);
+        }
+    }
+}
