6a68b1476dfcef4cfcb2fc0810f02e439752f868
Steven Yackel <spazard1@gmail.com>
2022-10-04 14:52:10 -0500

    fix null entity

--- Lectures/10-04-2022 async await/Mario/Mario/Services/ExternalMarioService.cs
+++ Lectures/10-04-2022 async await/Mario/Mario/Services/ExternalMarioService.cs
@@ -14,7 +14,7 @@ namespace Mario.Services
             var response = await httpClient.GetAsync("https://webprogrammingmario.azurewebsites.net/api/mario/jump");
             var responseString = await response.Content.ReadAsStringAsync();
 
-            return JsonConvert.DeserializeObject<MarioEntity>(responseString);
+            return JsonConvert.DeserializeObject<MarioEntity?>(responseString);
         }
     }
 }
