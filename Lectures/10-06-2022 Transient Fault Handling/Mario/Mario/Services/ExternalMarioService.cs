0a1939ae54ca2f9ef221f3ac72330e59176aeb12
Steven Yackel <spazard1@gmail.com>
2022-10-06 14:49:35 -0500

    adds TFH lecture

---
+++ Lectures/10-06-2022 Transient Fault Handling/Mario/Mario/Services/ExternalMarioService.cs
@@ -0,0 +1,43 @@
+﻿using Mario.Entities;
+using Newtonsoft.Json;
+using Polly;
+using System.Diagnostics;
+using System.Net;
+using System.Runtime.CompilerServices;
+
+namespace Mario.Services
+{
+    public class ExternalMarioService
+    {
+
+        private readonly HttpClient httpClient = new();
+
+        public async Task<MarioEntity?> GetMarioLevelStatusAsync()
+        {
+            var policy = Policy.HandleInner<HttpRequestException>(ex =>
+            {
+                return ex?.StatusCode == HttpStatusCode.ServiceUnavailable;
+            }).WaitAndRetryAsync(5, retryAttempt =>
+                TimeSpan.FromMilliseconds(100 * Math.Pow(2, retryAttempt))
+            ,(ex, timeSpan, context) =>
+            {
+                Debug.WriteLine("We are trying again.");
+            });
+
+            string? responseString = null;
+
+            await policy.ExecuteAsync(async () =>
+            {
+                var response = await httpClient.GetAsync("https://webprogrammingmario.azurewebsites.net/api/mario/jump");
+                responseString = await response.Content.ReadAsStringAsync();
+            });
+
+            if (responseString == null)
+            {
+                return null;
+            }
+
+            return JsonConvert.DeserializeObject<MarioEntity?>(responseString);
+        }
+    }
+}
