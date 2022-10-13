b9c054b41887c92e73f0f9c51cccf7c77578c7b7
Steven Yackel <spazard1@gmail.com>
2022-10-13 13:41:44 -0500

    adds base react 2 lecture

---
+++ Lectures/10-13-2022 React 2/clicker/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import ClickCounter from './components/ClickCounter';
+import NumberList from './components/NumberList';
+
+const App = () => {
+  return (
+    <div className="App">
+      Hello, World!
+      <ClickCounter />
+      <ClickCounter />
+      
+      <NumberList />
+    </div>
+  );
+}
+
+export default App;
---
+++ Lectures/10-13-2022 React 2/clicker/src/components/NumberList.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+
+import "./NumberList.css";
+
+
+const NumberList = () => {
+  const [numbers, setNumbers] = useState([]);
+
+  const addNumber = () => {
+    setNumbers([...numbers, Math.random()])
+  }
+
+  const deleteNumber = (index) => {
+    const newNumbers = [...numbers];
+    newNumbers.splice(index, 1);
+    setNumbers(newNumbers);
+  }
+
+  return (
+    <>
+      <Button onClick={addNumber}>Add Number</Button>
+      <div className="numberListContainer">
+        {numbers.map((element, index) => 
+          <div onClick={() => deleteNumber(index)}>{element}</div>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default NumberList;
