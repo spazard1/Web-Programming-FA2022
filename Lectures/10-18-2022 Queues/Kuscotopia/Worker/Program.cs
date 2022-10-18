a9e7128c783db9fff8c8849934a4cbef314728ca
Steven Yackel <spazard1@gmail.com>
2022-10-18 14:57:52 -0500

    adds queues lecture

---
+++ Lectures/10-18-2022 Queues/Kuscotopia/Worker/Program.cs
@@ -0,0 +1,50 @@
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using Common;
+
+namespace Worker
+{
+    internal class Program
+    {
+        private static BasicAWSCredentials credentials;
+        private static AmazonSQSClient sqsClient;
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Starting to read messages from the queue...");
+
+            credentials = new BasicAWSCredentials(Constants.QueueKeyId, Constants.QueueKey);
+            sqsClient = new AmazonSQSClient(credentials, RegionEndpoint.USEast1);
+
+            await ReadMessagesAsync();
+        }
+
+        public static async Task ReadMessagesAsync()
+        {
+            var request = new ReceiveMessageRequest()
+            {
+                QueueUrl = Constants.QueueUrl,
+                MaxNumberOfMessages = 10,
+                WaitTimeSeconds = 10
+            };
+
+            while (true)
+            {
+                var messages = await sqsClient.ReceiveMessageAsync(request);
+
+                foreach (var message in messages.Messages)
+                {
+                    Console.WriteLine(message.Body);
+
+                    _ = sqsClient.DeleteMessageAsync(new DeleteMessageRequest()
+                    {
+                        QueueUrl = Constants.QueueUrl,
+                        ReceiptHandle = message.ReceiptHandle
+                    });
+                }
+            }
+        }
+    }
+}
